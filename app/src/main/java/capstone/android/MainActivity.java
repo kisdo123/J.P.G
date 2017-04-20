3d71962348f4a9583929910c6c9964c2b365fc4f
kisdo123 <kisdo123@naver.com>
2017-04-20 15:52:39 +0900

    04.20  형렬 클릭시 카메라 갤러리

--- app/src/main/java/capstone/android/MainActivity.java
+++ app/src/main/java/capstone/android/MainActivity.java
@@ -1,13 +1,44 @@
 package capstone.android;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button cam = null;
+    Button gal = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setup();
+    }
+
+    private void setup() {
+        cam = (Button)findViewById(R.id.btn_cam);
+        gal = (Button)findViewById(R.id.btn_gallay);
+
+        cam.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,1);
+            }
+        });
+        gal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 }
