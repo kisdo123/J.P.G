ea014b09f1f96d52b18634ba2141e1b81e1c0d1b
kisdo123 <kisdo123@naver.com>
2017-04-26 15:17:48 +0900

    04.26 형렬 UI 갤러리 사진가져오기

--- app/src/main/java/capstone/android/MainActivity.java
+++ app/src/main/java/capstone/android/MainActivity.java
@@ -1,15 +1,25 @@
 package capstone.android;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int PICK_IMAGE_REQUEST = 1;
+    static final String TAG = "MainActivity";
+
+    ImageView imgView;
+
     Button cam = null;
-    Button gal = null;
+    Button gallery = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,26 +29,55 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setup() {
-        cam = (Button)findViewById(R.id.btn_cam);
-        gal = (Button)findViewById(R.id.btn_gallay);
+        cam = (Button) findViewById(R.id.btn_cam);
+        gallery = (Button) findViewById(R.id.btn_gallery);
 
-        cam.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,1);
-            }
-        });
-        gal.setOnClickListener(new View.OnClickListener() {
+        cam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_GET_CONTENT);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 1);
+            }
+        });
+
+        gallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
                 intent.setType("image/*");
-                startActivityForResult(intent, 0);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
             }
         });
     }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        try {
+            if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && null != data) {
+                Uri uri = data.getData();
+
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                int nh = (int) (bitmap.getHeight() * (1024.0 / bitmap.getWidth()));
+                Bitmap scaled = Bitmap.createScaledBitmap(bitmap, 1024, nh, true);
+
+                imgView = (ImageView) findViewById(R.id.imageView);
+                imgView.setImageBitmap(scaled);
+
+                Toast.makeText(this, "이미지 선택 완료", Toast.LENGTH_LONG).show();
+
+            } else {
+                Toast.makeText(this, "취소되었습니다.", Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(this, "불러오지 못했습니다.", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        }
+    }
+
+    public void onClick_change(View view) {
+        Intent intent = new Intent(this, OcrActivity.class);
+        startActivity(intent);
+    }
+
 }
